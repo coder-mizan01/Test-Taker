4a8999f2733dcb4df0039bb79c1e65f41a308fce
coder-mizan01 <p.mizan2441@gmail.com>
2023-09-10 12:11:46 +0600

    try 3

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -5,6 +5,7 @@ const Contact = () => {
     <>
       <section id="contact">
         <iframe
+         title="This is locaiton"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14602.795966348503!2d90.41629788405139!3d23.79373090386653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7a4f705a4b1%3A0xe0b5315081473ad!2z4Ka44Ka_4KaV4KeN4Ka4IOCmuOCmv-CmnOCmqOCmuCDgprngp4vgpp_gp4fgprI!5e0!3m2!1sbn!2sbd!4v1693371673828!5m2!1sbn!2sbd"
           style={{width:'100%',height:'500px'}}
           allowFullScreen=""
